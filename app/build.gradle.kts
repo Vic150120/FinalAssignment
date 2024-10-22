72d64669688c41bdc794e2ffc3591567e12c5745
phuct <phucthienhong@gmail.com>
2024-10-22 18:12:35 +1100

    Add Navbar bottom

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,11 +8,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.Final_Assignment"
+    namespace = "com.example.api_project_aurelio"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.Final_Assignment"
+        applicationId = "com.example.api_project_aurelio"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
@@ -51,19 +51,15 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    //
-    // Added dependencies
-    //
+
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.navigation:navigation-fragment-ktx:2.8.2")
     implementation("androidx.navigation:navigation-ui-ktx:2.8.2")
     implementation("androidx.compose.material3:material3:1.3.0")
-    // Add Retrofit
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-moshi:2.11.0")
     implementation("com.squareup.moshi:moshi-kotlin:1.14.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
-    // Hilt Framework - Dependency Injection
     implementation("com.google.dagger:hilt-android:2.51.1")
     kapt("com.google.dagger:hilt-android-compiler:2.51.1")
 }
