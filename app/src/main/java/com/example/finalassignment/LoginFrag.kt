4b40f01a2b13790272a797a1cdd29961c3a98e8e
phuct <phucthienhong@gmail.com>
2024-10-22 18:12:35 +1100

    Add Navbar bottom

--- app/src/main/java/com/example/finalassignment/LoginFrag.kt
+++ app/src/main/java/com/example/finalassignment/LoginFrag.kt
@@ -1,11 +1,11 @@
 package com.example.finalassignment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.Final_Assignment.R
+import androidx.fragment.app.Fragment
+import com.example.api_project_aurelio.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -14,7 +14,7 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [LoginFrag.newInstance] factory method to
+ * Use the [DetailsFrag.newInstance] factory method to
  * create an instance of this fragment.
  */
 class LoginFrag : Fragment() {
@@ -45,12 +45,12 @@ class LoginFrag : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment LoginFrag.
+         * @return A new instance of fragment DetailsFrag.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            LoginFrag().apply {
+            DetailsFrag().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
